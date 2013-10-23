7c2db06d840d3f8b0bf33546e01b01ba350955e7
florent claerhout <florent.claerhout@gmail.com>
2013-10-23 22:00:16 +0200

    increase font size

--- z.conf.tcl
+++ z.conf.tcl
@@ -12,14 +12,14 @@
 		set skin_abg #8dbf80
 		set skin_afg #FFFFFF
 
-	set font1 -adobe-helvetica-medium-r-normal-*-*-80-*-*-p-*-iso10646-1
-	set font2 -adobe-helvetica-medium-r-normal-*-*-100-*-*-p-*-iso10646-1
-	set font3 -adobe-helvetica-bold-r-normal-*-*-80-*-*-p-*-iso10646-1
-	set font4 -adobe-helvetica-bold-r-normal-*-*-100-*-*-p-*-iso10646-1
+	set font1 -adobe-helvetica-medium-r-normal-*-*-120-*-*-p-*-iso10646-1
+	set font2 -adobe-helvetica-medium-r-normal-*-*-140-*-*-p-*-iso10646-1
+	set font3 -adobe-helvetica-bold-r-normal-*-*-120-*-*-p-*-iso10646-1
+	set font4 -adobe-helvetica-bold-r-normal-*-*-140-*-*-p-*-iso10646-1
 	set tab 20
 	set defaultName scratch.zzz
-	set autoSwitch 1
+	set autoSwitch 0
 	set autoScroll 0
 	set helpFile Change.log
 	set fileListWidth 22
-	set usergeometry "740x532+301+89"
+	set usergeometry "740x532+387+25"
